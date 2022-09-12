aa0e365394b6df943d73c476e17a622bb0442b12
BreakTheCode1 <112843804+BreakTheCode1@users.noreply.github.com>
2022-09-12 22:22:20 +0530

    Create Template.java

---
+++ Template.java
@@ -0,0 +1,203 @@
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+
+/* Name of the class has to be "Main" only if the class is public. */
+public class temp
+{
+    public static void main (String[] args) throws Exception
+    {
+        // your code goes here
+        FastScanner sc=  new FastScanner();
+        int t = sc.nextInt();
+        while(t-->0){
+
+        }
+
+
+    }
+
+
+
+    public static boolean isPrime(long n)
+    {
+        if(n < 2) return false;
+        if(n == 2 || n == 3) return true;
+        if(n%2 == 0 || n%3 == 0) return false;
+        long sqrtN = (long)Math.sqrt(n)+1;
+        for(long i = 6L; i <= sqrtN; i += 6) {
+            if(n%(i-1) == 0 || n%(i+1) == 0) return false;
+        }
+        return true;
+    }
+
+    public static long gcd(long a, long b)
+    {
+        if(a > b)
+            a = (a+b)-(b=a);
+        if(a == 0L)
+            return b;
+        return gcd(b%a, a);
+    }
+
+    public static ArrayList<Integer> findDiv(int N)
+    {
+        //gens all divisors of N
+        ArrayList<Integer> ls1 = new ArrayList<Integer>();
+        ArrayList<Integer> ls2 = new ArrayList<Integer>();
+        for(int i=1; i <= (int)(Math.sqrt(N)+0.00000001); i++)
+            if(N%i == 0)
+            {
+                ls1.add(i);
+                ls2.add(N/i);
+            }
+        Collections.reverse(ls2);
+        for(int b: ls2)
+            if(b != ls1.get(ls1.size()-1))
+                ls1.add(b);
+        return ls1;
+    }
+    public static void sort(int[] arr)
+    {
+        //because Arrays.sort() uses quicksort which is dumb
+        //Collections.sort() uses merge sort
+        ArrayList<Integer> ls = new ArrayList<Integer>();
+        for(int x: arr)
+            ls.add(x);
+        Collections.sort(ls);
+        for(int i=0; i < arr.length; i++)
+            arr[i] = ls.get(i);
+    }
+    public static long power(long x, long y, long p)
+    {
+        //0^0 = 1
+        long res = 1L;
+        x = x%p;
+        while(y > 0)
+        {
+            if((y&1)==1)
+                res = (res*x)%p;
+            y >>= 1;
+            x = (x*x)%p;
+        }
+        return res;
+    }
+
+
+   static class FastScanner
+    {
+        //I don't understand how this works lmao
+        private int BS = 1 << 16;
+        private char NC = (char) 0;
+        private byte[] buf = new byte[BS];
+        private int bId = 0, size = 0;
+        private char c = NC;
+        private double cnt = 1;
+        private BufferedInputStream in;
+
+        public FastScanner() {
+            in = new BufferedInputStream(System.in, BS);
+        }
+
+        public FastScanner(String s) {
+            try {
+                in = new BufferedInputStream(new FileInputStream(new File(s)), BS);
+            } catch (Exception e) {
+                in = new BufferedInputStream(System.in, BS);
+            }
+        }
+
+        private char getChar() {
+            while (bId == size) {
+                try {
+                    size = in.read(buf);
+                } catch (Exception e) {
+                    return NC;
+                }
+                if (size == -1) return NC;
+                bId = 0;
+            }
+            return (char) buf[bId++];
+        }
+
+        public int nextInt() {
+            return (int) nextLong();
+        }
+
+        public int[] nextInts(int N) {
+            int[] res = new int[N];
+            for (int i = 0; i < N; i++) {
+                res[i] = (int) nextLong();
+            }
+            return res;
+        }
+
+        public long[] nextLongs(int N) {
+            long[] res = new long[N];
+            for (int i = 0; i < N; i++) {
+                res[i] = nextLong();
+            }
+            return res;
+        }
+
+        public long nextLong() {
+            cnt = 1;
+            boolean neg = false;
+            if (c == NC) c = getChar();
+            for (; (c < '0' || c > '9'); c = getChar()) {
+                if (c == '-') neg = true;
+            }
+            long res = 0;
+            for (; c >= '0' && c <= '9'; c = getChar()) {
+                res = (res << 3) + (res << 1) + c - '0';
+                cnt *= 10;
+            }
+            return neg ? -res : res;
+        }
+
+        public double nextDouble() {
+            double cur = nextLong();
+            return c != '.' ? cur : cur + nextLong() / cnt;
+        }
+
+        public double[] nextDoubles(int N) {
+            double[] res = new double[N];
+            for (int i = 0; i < N; i++) {
+                res[i] = nextDouble();
+            }
+            return res;
+        }
+
+        public String next() {
+            StringBuilder res = new StringBuilder();
+            while (c <= 32) c = getChar();
+            while (c > 32) {
+                res.append(c);
+                c = getChar();
+            }
+            return res.toString();
+        }
+
+        public String nextLine() {
+            StringBuilder res = new StringBuilder();
+            while (c <= 32) c = getChar();
+            while (c != '\n') {
+                res.append(c);
+                c = getChar();
+            }
+            return res.toString();
+        }
+
+        public boolean hasNext() {
+            if (c > 32) return true;
+            while (true) {
+                c = getChar();
+                if (c == NC) return false;
+                else if (c > 32) return true;
+            }
+        }
+    }
+}
